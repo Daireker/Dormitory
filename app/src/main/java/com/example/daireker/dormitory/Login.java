bc7904c7aaaaba9de2e3ba8bf5616bcaf5e446d6
Daireker <243930237@qq.com>
2017-11-01 17:27:56 +0800

    second commit

--- app/src/main/java/com/example/daireker/dormitory/Login.java
+++ app/src/main/java/com/example/daireker/dormitory/Login.java
@@ -4,17 +4,22 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.LinkMovementMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity implements View.OnClickListener{
 
+    public static boolean canSee = false;
+
     private EditText mID, mPassword;
 
-    private Button mLogin;
+    private Button mLogin, mCondition;
 
     private TextView mForget;
 
@@ -24,31 +29,55 @@ public class Login extends AppCompatActivity implements View.OnClickListener{
         setContentView(R.layout.login);
 
         initView();
-        mLogin = (Button) findViewById(R.id.login);
-        mLogin.setOnClickListener(this);
-
-        mForget = (TextView) findViewById(R.id.forget);
-        mForget.setText(Html.fromHtml("<a href='http://www.baidu.com'>忘记密码</a>"));
-        mForget.setMovementMethod(LinkMovementMethod.getInstance());
-        //mForget.setOnClickListener(this);
     }
 
     void initView(){
         mID = (EditText) findViewById(R.id.inputID);
+
         mPassword = (EditText) findViewById(R.id.inputPassword);
+        mPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+
+        mCondition = (Button) findViewById(R.id.condition);
+        mCondition.setOnClickListener(this);
+
+        mLogin = (Button) findViewById(R.id.login);
+        mLogin.setOnClickListener(this);
+
+        mForget = (TextView) findViewById(R.id.forget);
+        mForget.setText(Html.fromHtml("<a href='https://iaaa.pku.edu.cn/iaaa/pageFlows/authen/findPwd2.jsp'>忘记密码</a>"));
+        mForget.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.login:
-                Intent i = new Intent(this,MainActivity.class);
-                startActivity(i);
+                check();
                 break;
-            //case R.id.forget:
-            //  break;
+            case R.id.condition:
+                if(canSee == false){
+                    mPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    canSee = true;
+                }else {
+                    mPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    canSee = false;
+                }
+                break;
             default:
                 break;
+        }
+    }
+
+    public void check(){
+        if(mID.getText().toString().length()==0 ||
+                mPassword.getText().toString().length()==0){
+            Toast.makeText(Login.this,"请输入账户或密码",Toast.LENGTH_SHORT).show();
+        }else if (mID.getText().toString().equals("000000") &&
+                mPassword.getText().toString().equals("000000")){
+            Intent i = new Intent(this,MainActivity.class);
+            startActivity(i);
+        }else{
+            Toast.makeText(Login.this,"账户或密码不正确",Toast.LENGTH_SHORT).show();
         }
     }
 }
